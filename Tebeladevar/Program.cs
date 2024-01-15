1f76649b2408a0c174942e7efa469cef885e8c27
roberta7724 <roberta.bara@emanuel.ro>
2024-01-15 12:17:23 +0200

    tabel

---
+++ Tebeladevar/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Tebeladevar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine("| A | B | A & B |");
+            Console.WriteLine("|---|---|-------|");
+
+            for (int a = 0; a <= 1; a++)
+            {
+                for (int b = 0; b <= 1; b++)
+                {
+                    int result = a & b;
+                    Console.WriteLine($"| {a} | {b} |   {result}   |");
+                    Console.WriteLine("-----------------");
+                }
+            }
+        }
+    }
+}
